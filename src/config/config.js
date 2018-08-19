347560333f666632a12eed807985f3d047c949cf
Dane Williamson <daneandrew16@gmail.com>
2018-08-19 12:44:22 -0400

    changed config declaration and assignment

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,9 @@
 var env = process.env.NODE_ENV || 'development';
-var config = require('./config.json');
+
 
 if(env == 'development')
 {
+  var config = require('./config.json');
   var envConfig = config[env];
   Object.keys(envConfig).forEach((key) =>
   {
